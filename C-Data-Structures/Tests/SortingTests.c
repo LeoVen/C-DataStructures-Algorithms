c46b7f7000d3fda675b096d75751ccfc829f1258
Leonardo <lvenk26@gmail.com>
2018-04-18 23:35:40 -0300

    Re-added SortingTests

--- C-Data-Structures/Tests/SortingTests.c
+++ C-Data-Structures/Tests/SortingTests.c
@@ -23,27 +23,32 @@ int SortingTests() {
 	printf(" +-------------------------------------+\n");
 	printf("\n");
 
-	/*
+	
 	int i;
 
 	// Testing limits of Insertion Sort
-	Array *earray = getCArray(100000);
-	for (i = 0; i < earray->size; i++) {
-		insertValueCArray(earray, i, rand());
+	Array *array;
+	
+	arr_init(&array, 100000);
+
+	for (i = 0; i < array->size; i++) {
+		arr_insert(array, i, rand());
 	}
 
 	printf("\nInsertion Sort:");
 	clock_t begin = clock();
+
 	// Timing insertion sort
-	insertionSortCArray(earray);
+	arr_sort_insertion(array);
+
 	clock_t end = clock();
 	double time_spent4 = (double)(end - begin) / CLOCKS_PER_SEC;
 
 	printf("\nTotal time spent for insertion sort: %lf seconds", time_spent4);
-	// displayCArray(earray);
+	
+	//arr_display(array);
 
-	free(earray);
+	arr_delete(array);
 
-	*/
 	return 0;
 }
